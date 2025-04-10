6987df294a2ba7ff6854f7624e7f0347c6b34813
Fabio Luis Batista Santos <contatofabio2005@gmail.com>
2025-04-10 16:50:13 -0300

    Add files via upload

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,8 +1,8 @@
 import style from "./Contact.module.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
-import "leaflet-defaulticon-compatibility";
+// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+// import "leaflet/dist/leaflet.css";
+// import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+// import "leaflet-defaulticon-compatibility";
 import { Menu } from "./components/menu";
 
 function Contact() {
